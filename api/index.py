eb2b37cf61bf91416a22daa7d593c58b96dbba14
Youngeun Seol <jeyejong70@naver.com>
2026-03-26 17:03:09 +0900

    🚀 vercel 배포용 작업 (#10)
    * ✨ 계산기 기능 구현 (#9)
    
    * 🐛 route 수정 (#9)
    
    * 🐛 배포 버그 수정 (#9)
    
    * 🐛 route 재수정 (#9)
    
    * 🚚 폴더 정리 (#9)
    
    * 🚚 json 파일 이동 (#9)
    
    * 🐛 롤백 (#9)

---
+++ api/index.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder="../templates")
+
+@app.route("/api/calculator", methods=["GET", "POST"])
+def home():
+    result = None
+    error = None
+
+    if request.method == "POST":
+        num1 = request.form.get("num1")
+        num2 = request.form.get("num2")
+
+        try:
+            result = int(num1) + int(num2)
+        except:
+            error = "숫자를 올바르게 입력해주세요."
+
+    return render_template("calculator.html", result=result, error=error)
+
+app = app
