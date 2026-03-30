439786d77a6c7ae6d89c6992aadf84b8a96e8252
Youngeun Seol <jeyejong70@naver.com>
2026-03-30 17:32:36 +0900

    🐛 서버 에러 해결
    Refactor database connection handling and error management in init_db, insert_log, and get_logs functions. Update calculator logic to handle float inputs.

--- api/index.py
+++ api/index.py
@@ -8,6 +8,10 @@ app = Flask(__name__, template_folder="../templates")
 
 def get_conn():
     url = os.environ.get("DATABASE_URL")
+
+    if not url:
+        raise RuntimeError("DATABASE_URL 없음")
+
     p = urlparse(url)
     ctx = ssl.create_default_context()
 
@@ -21,52 +25,61 @@ def get_conn():
     )
 
 def init_db():
-    conn = get_conn()
-    conn.run("""
-        CREATE TABLE IF NOT EXISTS calc_logs (
-            id SERIAL PRIMARY KEY,
-            num1 FLOAT,
-            num2 FLOAT,
-            result FLOAT,
-            created_at TIMESTAMPTZ DEFAULT NOW()
-        )
-    """)
-    conn.close()
+    try:
+        conn = get_conn()
+        conn.run("""
+            CREATE TABLE IF NOT EXISTS calc_logs (
+                id SERIAL PRIMARY KEY,
+                num1 FLOAT,
+                num2 FLOAT,
+                result FLOAT,
+                created_at TIMESTAMPTZ DEFAULT NOW()
+            )
+        """)
+        conn.close()
+    except Exception as e:
+        print("INIT DB ERROR:", e)
 
 def insert_log(a, b, result):
-    conn = get_conn()
-    conn.run(
-        "INSERT INTO calc_logs (num1, num2, result) VALUES (:a, :b, :r)",
-        a=a, b=b, r=result
-    )
-    conn.close()
+    try:
+        conn = get_conn()
+        conn.run(
+            "INSERT INTO calc_logs (num1, num2, result) VALUES (:a, :b, :r)",
+            a=a, b=b, r=result
+        )
+        conn.close()
+    except Exception as e:
+        print("INSERT ERROR:", e)
 
 def get_logs():
-    conn = get_conn()
-    rows = conn.run("""
-        SELECT id, created_at, num1, num2, result
-        FROM calc_logs
-        ORDER BY id DESC
-        LIMIT 10
-    """)
-    conn.close()
-    return rows
+    try:
+        conn = get_conn()
+        rows = conn.run("""
+            SELECT id, created_at, num1, num2, result
+            FROM calc_logs
+            ORDER BY id DESC
+            LIMIT 10
+        """)
+        conn.close()
+        return rows
+    except Exception as e:
+        print("GET LOG ERROR:", e)
+        return []
+
+init_db()
 
 @app.route("/api/calculator", methods=["GET", "POST"])
 def home():
     result = None
     error = None
 
-    init_db()
-
     if request.method == "POST":
         num1 = request.form.get("num1")
         num2 = request.form.get("num2")
 
         try:
-            result = int(num1) + int(num2)
-
-            insert_log(int(num1), int(num2), result)
+            result = float(num1) + float(num2)
+            insert_log(float(num1), float(num2), result)
 
         except:
             error = "숫자를 올바르게 입력해주세요."
