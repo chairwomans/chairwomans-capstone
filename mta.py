e48b25d501431a06ab870f378ec23e30af39c17c
JIMIN <zziminally@gmail.com>
2026-05-28 15:32:23 +0900

    ✨ 가우시안 커널 함수 구현 (#34)
    Add a function to create multiple views of an image using transformations.

--- mta.py
+++ mta.py
@@ -1,1 +1,25 @@
+import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
 
+
+def make_views(image, n_views):
+    """return one original image + RandomResizedCrop n_views"""
+    normalize = transforms.Normalize(
+        mean=[0.48145466, 0.4578275, 0.40821073],
+        std=[0.26862954, 0.26130258, 0.27577711]
+    )
+    base_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    crop_transform = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    original = base_transform(image).unsqueeze(0)
+    crops = torch.stack([crop_transform(image) for _ in range(n_views)])
+    return torch.cat([original, crops], dim=0)
